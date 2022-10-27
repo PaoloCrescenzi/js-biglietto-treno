a875a85c4492da4f49cc0cf36e6371799858a6bf
PaoloCrescenz <crescenzipaolo2@gmail.com>
2022-10-27 15:30:33 +0200

    creazione file + prompt

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const userAge = prompt("Inserisci la tua età")
+const chilometri = prompt("Inserisci il numero di chilometri che vuoi percorrere")
+const costoTicket = .21
+
+
+console.log("Eta passeggero:", userAge)
+console.log("Lunghezza trata", chilometri)
+
+if( userAge < 18 ){
+    console.log("L'utente è minorenne")
+}
